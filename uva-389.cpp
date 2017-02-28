905e49e8c6fe9cc2e65c1a67d73f12093110bb6b
Yestin <641010761@qq.com>
2017-02-28 23:13:36 +0800

    0

---
+++ uva-389.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+
+using namespace std;
+
+char ch[40];
+
+int change1(char c)
+{
+	if(c>='0'&&c<='9')
+		return c-'0';
+	else
+		return c-'A'+10;
+}
+
+int change2(char c)
+{
+	if(c<10)
+		return c+'0';
+	else
+		return c-10+'A';
+}
+
+main()
+{
+	int base1,base2;
+	while(cin>>ch>>base1>>base2)
+	{
+		int value=0,move=0,save=0;
+		while(ch[move])
+		{
+			value*=base1;
+			value+=change1(ch[move++]);
+		}
+		if(!value)
+			ch[save++]='0';
+		while(value)
+		{
+			ch[save++]=change2(value%base2)
+			value/=base2;
+		}
+		if(save>7)
+			cout<<"ERROR"<<endl;
+		else
+		{
+			for(int i=save;i<7;++i)
+				cout<<" ";
+			while(--save>=0)
+				cout<<ch[save];
+			cout<<endl;
+		}
+	}
+}
